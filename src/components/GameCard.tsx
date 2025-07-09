fb96d4780ef3bccd9bcd6f82e35d2cb064f0bd20
Jonathan Adebo <kent93226@gmail.com>
2025-07-09 23:51:55 +0100

    Displaying criticScore

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,26 @@
+import type { Game } from "@/hooks/useGames";
+import { Card, HStack, Image } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+
+interface Props {
+  game: Game;
+}
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card.Root width="320px" key={game.id} borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <Card.Body gap="2">
+        <Card.Header fontSize="2xl">{game.name}</Card.Header>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </Card.Body>
+    </Card.Root>
+  );
+};
+
+export default GameCard;
