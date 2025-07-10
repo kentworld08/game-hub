aed82bf407ac29cc25cb23a1d502f29388b6b71a
Jonathan Adebo <kent93226@gmail.com>
2025-07-11 00:09:59 +0100

    clean up the genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,6 +2,7 @@ import useGenres, { type Genre } from "@/hooks/useGenres";
 import GetCroppedImageUrl from "@/services/image-url";
 import {
   Button,
+  Heading,
   HStack,
   Image,
   List,
@@ -20,29 +21,36 @@ const GenreList = ({ onselectGenre, selectedGenre }: Props) => {
   if (isLoading) return <Spinner />;
 
   return (
-    <List.Root>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px" listStyle="none">
-          <HStack>
-            <Image
-              src={GetCroppedImageUrl(genre.image_background)}
-              boxSize="32px"
-              borderRadius={8}
-            />
-            <Button
-              onClick={() => onselectGenre(genre)}
-              margin={0}
-              padding={0}
-              variant="ghost"
-              fontSize="sm"
-              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List.Root>
+    <>
+      <Heading fontWeight="bold" marginBottom={3} fontSize="2xl">
+        Genre
+      </Heading>
+      <List.Root>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px" listStyle="none">
+            <HStack>
+              <Image
+                src={GetCroppedImageUrl(genre.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                alt={`${genre.name} icon`}
+              />
+              <Button
+                onClick={() => onselectGenre(genre)}
+                margin={0}
+                padding={0}
+                variant="ghost"
+                fontSize="sm"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List.Root>
+    </>
   );
 };
 
