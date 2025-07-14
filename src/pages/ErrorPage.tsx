6159f40bfce80318135840e797704970a48e3857
Jonathan Adebo <kent93226@gmail.com>
2025-07-14 16:50:26 +0100

    Handle error

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import NavBar from "@/components/NavBar";
+import { Box, GridItem, Heading, Text } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+export const ErrorPage = () => {
+  const error = useRouteError();
+  return (
+    <>
+      <GridItem area="nav" position="sticky" top={0} zIndex={5} bg={"gray.900"}>
+        <NavBar />
+      </GridItem>
+      <Box padding={5}>
+        <Heading>Oops!</Heading>
+        <Text>
+          {" "}
+          {isRouteErrorResponse(error)
+            ? "This page does not exist."
+            : "An unexpected error occured."}
+        </Text>
+      </Box>
+    </>
+  );
+};
